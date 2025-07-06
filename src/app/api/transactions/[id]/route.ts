3aa1feb52c9934ec6f529e02a4fc6a14584cd210
Ayush Rastogi <arastogi5422@gmail.com>
2025-07-06 14:27:54 +0530

    route.ts

--- src/app/api/transactions/[id]/route.ts
+++ src/app/api/transactions/[id]/route.ts
@@ -1,21 +1,26 @@
 import { connectDB } from "@/lib/db";
-import {Transaction} from "@/models/Transaction";
+import { Transaction } from "@/models/Transaction";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req: NextRequest, context: any) {
+export async function DELETE(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
   await connectDB();
 
-  const id = context.params.id; // ✅ No warning
+  const { id } = await context.params;
   await Transaction.findByIdAndDelete(id);
 
   return NextResponse.json({ success: true });
 }
 
-
-export async function PUT(req: NextRequest, context: any) {
+export async function PUT(
+  req: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
   await connectDB();
 
-  const id = context.params.id;
+  const { id } = await context.params;
   const data = await req.json();
 
   const updated = await Transaction.findByIdAndUpdate(id, data, { new: true });
